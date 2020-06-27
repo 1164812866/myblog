326aa29885628b1052440935c5c358f02219bd1a
1164812866 <1164812866@qq.com>
2020-06-27 16:41:43 +0800

    开发版本

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render,get_object_or_404
 from django.views import generic
 from .models import Article,Category,Tag,Timeline
 from django.conf import settings
-from django.core.cache import cache# redis缓存
 
 import markdown
 import time, datetime
@@ -43,37 +42,13 @@ class DetailView(generic.DetailView):
 
     def get_object(self):
         obj = super(DetailView, self).get_object()
-        # 设置浏览量增加时间判断,同一篇文章两次浏览超过半小时才重新统计阅览量,作者浏览忽略
-        u = self.request.user
-        ses = self.request.session
-        the_key = 'is_read_{}'.format(obj.id)
-        is_read_time = ses.get(the_key)
-        if u != obj.author:
-            if not is_read_time:
-                obj.update_views()
-                ses[the_key] = time.time()
-            else:
-                now_time = time.time()
-                t = now_time - is_read_time
-                if t > 60 * 30:
-                    obj.update_views()
-                    ses[the_key] = time.time()
-        # 获取文章更新的时间，判断是否从缓存中取文章的markdown,可以避免每次都转换
-        ud = obj.update_date.strftime("%Y%m%d%H%M%S")
-        md_key = '{}_md_{}'.format(obj.id, ud)
-        cache_md = cache.get(md_key)
-        if cache_md:
-            md = cache_md
-        else:
-            md = markdown.Markdown(extensions=[
-            'markdown.extensions.extra',
-            'markdown.extensions.codehilite',
-            'markdown.extensions.toc',
-            ])
-            obj.body = md.convert(obj.body)
-            obj.toc = md.toc
-            # 关键在这个地方，不能直接把 md 存进去
-            cache.set(md_key, (obj.body, obj.toc), 60 * 60 * 12)       
+        md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        'markdown.extensions.toc',
+        ])
+        obj.body = md.convert(obj.body)
+        obj.toc = md.toc  
         return obj
 
 # 分类
